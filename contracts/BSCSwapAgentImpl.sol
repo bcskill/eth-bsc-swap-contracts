fb89adb15368bc807fc543279d82cadf1864ae7f
Surou <652511569@qq.com>
2021-08-27 15:05:56 +0800

    update# code

--- contracts/BSCSwapAgentImpl.sol
+++ contracts/BSCSwapAgentImpl.sol
@@ -1,6 +1,7 @@
 pragma solidity 0.6.4;
 
 import "./interfaces/ISwap.sol";
+import "./interfaces/IERC20Query.sol";
 import "./bep20/BEP20UpgradeableProxy.sol";
 import './interfaces/IProxyInitialize.sol';
 import "openzeppelin-solidity/contracts/token/ERC20/SafeERC20.sol";
@@ -12,9 +13,10 @@ contract  BSCSwapAgentImpl is Context, Initializable {
 
     using SafeERC20 for IERC20;
 
-    mapping(address => address) public swapMappingETH2BSC;
-    mapping(address => address) public swapMappingBSC2ETH;
+    mapping(address => address) public swapMappingMain2Side;
+    mapping(address => address) public swapMappingSide2Main;
     mapping(bytes32 => bool) public filledETHTx;
+    mapping(bytes32 => bool) public createSwapPairTx;
 
     address payable public owner;
     address public bep20ProxyAdmin;
@@ -22,7 +24,7 @@ contract  BSCSwapAgentImpl is Context, Initializable {
     uint256 public swapFee;
 
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-    event SwapPairCreated(bytes32 indexed ethRegisterTxHash, address indexed bep20Addr, address indexed erc20Addr, string symbol, string name, uint8 decimals);
+    event SwapPairCreated(bytes32 indexed mainChainTxHash, address indexed mainChainErc20Addr, address indexed sideChainErc20Addr, string name, string symbol, uint8 decimals);
     event SwapStarted(address indexed bep20Addr, address indexed erc20Addr, address indexed fromAddr, uint256 amount, uint256 feeAmount);
     event SwapFilled(address indexed bep20Addr, bytes32 indexed ethTxHash, address indexed toAddress, uint256 amount);
 
@@ -90,18 +92,24 @@ contract  BSCSwapAgentImpl is Context, Initializable {
     /**
      * @dev createSwapPair
      */
-    function createSwapPair(bytes32 ethTxHash, address erc20Addr, string calldata name, string calldata symbol, uint8 decimals) onlyOwner external returns (address) {
-        require(swapMappingETH2BSC[erc20Addr] == address(0x0), "duplicated swap pair");
+    function createSwapPair(bytes32 mainChainTxHash, address mainChainErc20Addr, address sideChainErc20Addr, string calldata name, string calldata symbol, uint8 decimals) onlyOwner external returns (bool) {
+        require(!createSwapPairTx[mainChainTxHash], "main chain tx hash created already");
+        require(swapMappingMain2Side[mainChainErc20Addr] == address(0x0), "duplicated main chain swap pair");
+        require(swapMappingSide2Main[sideChainErc20Addr] == address(0x0), "duplicated side chain  swap pair");
 
-        BEP20UpgradeableProxy proxyToken = new BEP20UpgradeableProxy(bep20Implementation, bep20ProxyAdmin, "");
-        IProxyInitialize token = IProxyInitialize(address(proxyToken));
-        token.initialize(name, symbol, decimals, 0, true, address(this));
+        string memory mainChainErc20Name = IERC20Query(mainChainErc20Addr).name();
+        string memory mainChainErc20Symbol = IERC20Query(mainChainErc20Addr).symbol();
+        uint8 mainChainErc20Decimals = IERC20Query(mainChainErc20Addr).decimals();
 
-        swapMappingETH2BSC[erc20Addr] = address(token);
-        swapMappingBSC2ETH[address(token)] = erc20Addr;
+        require(keccak256(abi.encodePacked(mainChainErc20Name)) == keccak256(abi.encodePacked(name)), "main chain tx hash created already");
+        require(keccak256(abi.encodePacked(mainChainErc20Symbol)) == keccak256(abi.encodePacked(symbol)), "main chain tx hash created already");
+        require(mainChainErc20Decimals == decimals, "main chain tx hash created already");
 
-        emit SwapPairCreated(ethTxHash, address(token), erc20Addr, symbol, name, decimals);
-        return address(token);
+        swapMappingMain2Side[mainChainErc20Addr] = sideChainErc20Addr;
+        swapMappingSide2Main[sideChainErc20Addr] = mainChainErc20Addr;
+
+        emit SwapPairCreated(mainChainTxHash, mainChainErc20Addr, sideChainErc20Addr, name, symbol, decimals);
+        return true;
     }
 
     /**
@@ -109,7 +117,7 @@ contract  BSCSwapAgentImpl is Context, Initializable {
      */
     function fillETH2BSCSwap(bytes32 ethTxHash, address erc20Addr, address toAddress, uint256 amount) onlyOwner external returns (bool) {
         require(!filledETHTx[ethTxHash], "eth tx filled already");
-        address bscTokenAddr = swapMappingETH2BSC[erc20Addr];
+        address bscTokenAddr = swapMappingMain2Side[erc20Addr];
         require(bscTokenAddr != address(0x0), "no swap pair for this token");
         filledETHTx[ethTxHash] = true;
         ISwap(bscTokenAddr).mintTo(amount, toAddress);
@@ -121,7 +129,7 @@ contract  BSCSwapAgentImpl is Context, Initializable {
      * @dev swapBSC2ETH
      */
     function swapBSC2ETH(address bep20Addr, uint256 amount) payable external notContract returns (bool) {
-        address erc20Addr = swapMappingBSC2ETH[bep20Addr];
+        address erc20Addr = swapMappingSide2Main[bep20Addr];
         require(erc20Addr != address(0x0), "no swap pair for this token");
         require(msg.value == swapFee, "swap fee not equal");
 

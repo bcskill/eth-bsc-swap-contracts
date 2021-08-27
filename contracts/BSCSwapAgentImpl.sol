3ada32f37df3508e00d9343aa215eed68da14de0
Surou <652511569@qq.com>
2021-08-27 17:03:04 +0800

    update# code

--- contracts/BSCSwapAgentImpl.sol
+++ contracts/BSCSwapAgentImpl.sol
@@ -15,7 +15,7 @@ contract  BSCSwapAgentImpl is Context, Initializable {
 
     mapping(address => address) public swapMappingMain2Side;
     mapping(address => address) public swapMappingSide2Main;
-    mapping(bytes32 => bool) public filledETHTx;
+    mapping(bytes32 => bool) public filledMainTx;
     mapping(bytes32 => bool) public createSwapPairTx;
 
     address payable public owner;
@@ -25,15 +25,12 @@ contract  BSCSwapAgentImpl is Context, Initializable {
 
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
     event SwapPairCreated(bytes32 indexed mainChainTxHash, address indexed mainChainErc20Addr, address indexed sideChainErc20Addr, string name, string symbol, uint8 decimals);
-    event SwapStarted(address indexed bep20Addr, address indexed erc20Addr, address indexed fromAddr, uint256 amount, uint256 feeAmount);
-    event SwapFilled(address indexed bep20Addr, bytes32 indexed ethTxHash, address indexed toAddress, uint256 amount);
+    event SwapStarted(address indexed sideChainErc20Addr, address indexed mainChainErc20Addr, address indexed fromAddr, uint256 amount, uint256 feeAmount);
+    event SwapFilled(bytes32 indexed mainChainTxHash, address indexed mainChainErc20Addr, address indexed sideChainToAddr, uint256 amount);
 
     constructor() public {
     }
 
-    /**
-     * @dev Throws if called by any account other than the owner.
-     */
     modifier onlyOwner() {
         require(owner == _msgSender(), "Ownable: caller is not the owner");
         _;
@@ -44,8 +41,6 @@ contract  BSCSwapAgentImpl is Context, Initializable {
         require(msg.sender == tx.origin, "no proxy contract is allowed");
        _;
     }
-
-
 
     function initialize(address bep20Impl, uint256 fee, address payable ownerAddr, address bep20ProxyAdminAddr) public initializer {
         bep20Implementation = bep20Impl;
@@ -60,38 +55,21 @@ contract  BSCSwapAgentImpl is Context, Initializable {
         return size > 0;
     }
 
-    /**
-    * @dev Leaves the contract without owner. It will not be possible to call
-    * `onlyOwner` functions anymore. Can only be called by the current owner.
-    *
-    * NOTE: Renouncing ownership will leave the contract without an owner,
-    * thereby removing any functionality that is only available to the owner.
-    */
     function renounceOwnership() public onlyOwner {
         emit OwnershipTransferred(owner, address(0));
         owner = address(0);
     }
 
-    /**
-     * @dev Transfers ownership of the contract to a new account (`newOwner`).
-     * Can only be called by the current owner.
-     */
     function transferOwnership(address payable newOwner) public onlyOwner {
         require(newOwner != address(0), "Ownable: new owner is the zero address");
         emit OwnershipTransferred(owner, newOwner);
         owner = newOwner;
     }
 
-    /**
-     * @dev Returns set minimum swap fee from BEP20 to ERC20
-     */
     function setSwapFee(uint256 fee) onlyOwner external {
         swapFee = fee;
     }
 
-    /**
-     * @dev createSwapPair
-     */
     function createSwapPair(bytes32 mainChainTxHash, address mainChainErc20Addr, address sideChainErc20Addr, string calldata name, string calldata symbol, uint8 decimals) onlyOwner external returns (bool) {
         require(!createSwapPairTx[mainChainTxHash], "main chain tx hash created already");
         require(swapMappingMain2Side[mainChainErc20Addr] == address(0x0), "duplicated main chain swap pair");
@@ -107,39 +85,33 @@ contract  BSCSwapAgentImpl is Context, Initializable {
 
         swapMappingMain2Side[mainChainErc20Addr] = sideChainErc20Addr;
         swapMappingSide2Main[sideChainErc20Addr] = mainChainErc20Addr;
+        createSwapPairTx[mainChainTxHash] = true;
 
         emit SwapPairCreated(mainChainTxHash, mainChainErc20Addr, sideChainErc20Addr, name, symbol, decimals);
         return true;
     }
 
-    /**
-     * @dev fillETH2BSCSwap
-     */
-    function fillETH2BSCSwap(bytes32 ethTxHash, address erc20Addr, address toAddress, uint256 amount) onlyOwner external returns (bool) {
-        require(!filledETHTx[ethTxHash], "eth tx filled already");
-        address bscTokenAddr = swapMappingMain2Side[erc20Addr];
-        require(bscTokenAddr != address(0x0), "no swap pair for this token");
-        filledETHTx[ethTxHash] = true;
-        ISwap(bscTokenAddr).mintTo(amount, toAddress);
-        emit SwapFilled(bscTokenAddr, ethTxHash, toAddress, amount);
+    function fillMain2SideSwap(bytes32 mainChainTxHash, address mainChainErc20Addr, address sideChainToAddr, uint256 amount) onlyOwner payable external returns (bool) {
+        require(!filledMainTx[mainChainTxHash], "eth tx filled already");
+        address sideChainErc20Addr = swapMappingMain2Side[mainChainErc20Addr];
+        require(sideChainErc20Addr != address(0x0), "no swap pair for this token");
+        require(IERC20(sideChainErc20Addr).balanceOf(msg.sender) >= amount, "Insufficient contract account balance");
 
+        IERC20(sideChainErc20Addr).safeTransferFrom(msg.sender, sideChainToAddr, amount);
+        filledMainTx[mainChainTxHash] = true;
+
+        emit SwapFilled(mainChainTxHash, sideChainErc20Addr, sideChainToAddr, amount);
         return true;
     }
-    /**
-     * @dev swapBSC2ETH
-     */
-    function swapBSC2ETH(address bep20Addr, uint256 amount) payable external notContract returns (bool) {
-        address erc20Addr = swapMappingSide2Main[bep20Addr];
-        require(erc20Addr != address(0x0), "no swap pair for this token");
+ 
+    function swapSide2Main(address sideChainErc20Addr, uint256 amount) payable external notContract returns (bool) {
+        address mainChainErc20Addr = swapMappingSide2Main[sideChainErc20Addr];
+        require(mainChainErc20Addr != address(0x0), "no swap pair for this token");
         require(msg.value == swapFee, "swap fee not equal");
 
-        IERC20(bep20Addr).safeTransferFrom(msg.sender, address(this), amount);
-        ISwap(bep20Addr).burn(amount);
-        if (msg.value != 0) {
-            owner.transfer(msg.value);
-        }
+        IERC20(sideChainErc20Addr).safeTransferFrom(msg.sender, address(this), amount);
 
-        emit SwapStarted(bep20Addr, erc20Addr, msg.sender, amount, msg.value);
+        emit SwapStarted(sideChainErc20Addr, mainChainErc20Addr, msg.sender, amount, msg.value);
         return true;
     }
 }
